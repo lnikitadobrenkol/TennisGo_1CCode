ba58798cecf3298d9f66df5866de34c52576fe4b
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2020-10-10 13:16:03 +0300

    Test Case
    Test reader with exmaple data

---
+++ TennisGo_1CCode/Assets/Scripts/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FileReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+
+    void Start()
+    {
+        Employees employeesInJson = JsonUtility.FromJson<Employees>(jsonFile.text);
+
+        foreach (Employee employee in employeesInJson.employees)
+        {
+            Debug.Log("Found employee: " + employee.firstName + " " + employee.lastName);
+        }
+    }
+}
